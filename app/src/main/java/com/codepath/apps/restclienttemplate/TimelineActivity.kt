11dde9cc2a8b3bcd2f89f39c845afecece6dc376
pgoce <goce.partaloski@gmail.com>
2022-10-14 13:16:14 -0400

    Part 2

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -1,25 +1,29 @@
 package com.codepath.apps.restclienttemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import okhttp3.Headers
 import org.json.JSONException
 
 class TimelineActivity : AppCompatActivity() {
 
     lateinit var client: TwitterClient
-
     lateinit var rvTweets: RecyclerView
-
     lateinit var  adapter: TweetsAdapter
-
     lateinit var swipeContainer: SwipeRefreshLayout
+
+    lateinit var btnCompose : FloatingActionButton
 
     val tweets = ArrayList<Tweet>()
 
@@ -49,8 +53,41 @@ class TimelineActivity : AppCompatActivity() {
         rvTweets.layoutManager = LinearLayoutManager(this)
         rvTweets.adapter = adapter
 
+        btnCompose = findViewById(R.id.btnCompose)
+        btnCompose.setOnClickListener {
+            val intent = Intent(this, ComposeActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+
         populateHomeTimeline()
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
+//
+//        // Handles clicks on menu item
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if(item.itemId == R.id.compose) {
+//            val intent = Intent(this, ComposeActivity::class.java)
+//            startActivity(intent)
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            val tweet = data?.getParcelableExtra("tweet") as Tweet
+            tweets.add(0, tweet)
+            adapter.notifyItemInserted(0)
+            rvTweets.smoothScrollToPosition(0)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+
 
     fun populateHomeTimeline() {
         client.getHomeTimeline(object : JsonHttpResponseHandler() {
@@ -86,6 +123,7 @@ class TimelineActivity : AppCompatActivity() {
 
     companion object{
         val TAG = "TimelineActivity"
+        val REQUEST_CODE = 10
     }
 
 }
